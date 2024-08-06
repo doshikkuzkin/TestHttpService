e212dd4e756f9593414cf0cf024032d9c2c77a52
doshikkuzkin <doshikkuzkin@gmail.com>
2024-08-06 13:35:43 +0300

    Add initial service implementation

---
+++ TestHttpService/Assets/Scripts/Event.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+internal class Event
+{
+	[JsonProperty("type")]
+	public string Type { get; }
+
+	[JsonProperty("content")]
+	public string Content { get; }
+
+	public Event(string type, string content)
+	{
+		Type = type;
+		Content = content;
+	}
+}
